676789a0413e5488e2ef52c6897da329a42c3098
danielpkhalil <117323103+danielpkhalil@users.noreply.github.com>
2024-05-03 01:57:02 -0400

    Add files via upload

---
+++ pretrained_test.py
@@ -0,0 +1,93 @@
+from models.CNN import CNNModel
+from pretrained_test_dataset import TestDataset
+
+import os
+import csv
+import torch
+import torchvision
+from torch.utils.data import DataLoader
+from torchvision import transforms
+from torchvision.transforms import ToTensor, Resize, Compose, Lambda
+
+import torchxrayvision as xrv
+import skimage
+
+import pytorch_lightning as pl
+from pytorch_lightning.callbacks import ModelCheckpoint
+import argparse
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--path", type=str, required=True, help="Path to the test data folder")
+parser.add_argument("--checkpoint", type=str, required=True, help="Model checkpoint name")
+args = parser.parse_args()
+
+transform = torchvision.transforms.Compose([xrv.datasets.XRayCenterCrop(),xrv.datasets.XRayResizer(224)])
+
+test_dataset = TestDataset(root_dir=args.path, transform=transform)
+test_loader = DataLoader(test_dataset, batch_size=1, shuffle=False)
+
+model = xrv.models.DenseNet(weights=args.checkpoint)
+
+pathologies = ['No Finding', 'Enlarged Cardiomediastinum', 'Cardiomegaly', 'Lung Opacity', 'Pneumonia', 'Pleural Effusion', 'Pleural Other', 'Fracture', 'Support Devices']
+model_output_pathologies = {
+    'Atelectasis': [0],
+    'Consolidation': [1],
+    '': [2, 5, 6, 10, 11, 12, 13],
+    'Pneumothorax': [3],
+    'Edema': [4],
+    'Pleural Effusion': [7],
+    'Pneumonia': [8],
+    'Cardiomegaly': [10],
+    'Lung Lesion': [14],
+    'Fracture': [15],
+    'Lung Opacity': [16],
+    'Enlarged Cardiomediastinum': [17]
+}
+
+index_list = []
+for i, p in enumerate(pathologies):
+  if p in model_output_pathologies:
+    index_list.append(model_output_pathologies[p][0])
+  else:
+    index_list.append(0)
+
+def sort_predictions(avg_pred, index_list):
+    sorted_pred = [0] * 9
+    for i in range(len(index_list)):
+        if index_list[i] == 0:
+            sorted_pred[i] = 0.0
+        else:
+            # scale values to -1,1
+            sorted_pred[i] = (2*avg_pred[index_list[i]])-1
+    return sorted_pred
+
+pids = []
+predictions = []
+
+with torch.no_grad():
+    for pid, patient_images in test_loader:
+        patient_outputs = []
+        for image in patient_images:
+            output = model(image)
+            # raw score for each of the 9 labels
+            scores = output.squeeze().tolist()  # remove batch dimension
+            patient_outputs.append(scores)
+
+        # average predictions for each patient
+        avg_prediction = [sum(x) / len(x) for x in zip(*patient_outputs)]
+
+        pids.append(pid)
+        predictions.append(sort_predictions(avg_prediction, index_list))
+        print(predictions[-1])
+
+# save sorted predictions in csv
+sorted_predictions = sorted(zip(pids, predictions))
+labels = ['No Finding', 'Enlarged Cardiomediastinum', 'Cardiomegaly', 'Lung Opacity', 'Pneumonia', 'Pleural Effusion',
+          'Pleural Other', 'Fracture', 'Support Devices']
+
+with open('predictions.csv', 'w', newline='') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(['Id'] + labels)
+    for pid, prediction in sorted_predictions:
+        writer.writerow([pid.item()] + prediction)
---
+++ pretrained_test_dataset.py
@@ -0,0 +1,42 @@
+import os
+from PIL import Image
+from torchvision.transforms import ToTensor, Resize, Compose, Lambda
+from torch.utils.data import Dataset, DataLoader
+import torchxrayvision as xrv
+import skimage
+
+class TestDataset(Dataset):
+    def __init__(self, root_dir, transform=None):
+        self.root_dir = root_dir
+        self.transform = transform
+        self.patient_dirs = [d for d in os.listdir(self.root_dir) if os.path.isdir(os.path.join(self.root_dir, d))]
+
+    def __len__(self):
+        return len(self.patient_dirs)
+
+    def __getitem__(self, index):
+        patient_dir = self.patient_dirs[index]
+        patient_path = os.path.join(self.root_dir, patient_dir)
+
+        image_paths = []
+        for dirpath, _, filenames in os.walk(patient_path):
+            for filename in filenames:
+                if filename.endswith('.jpg'):
+                    image_paths.append(os.path.join(dirpath, filename))
+
+        images = []
+        for img_path in image_paths:
+            #image = Image.open(img_path)
+            image = skimage.io.imread(img_path, as_gray=False)
+
+            image = xrv.datasets.normalize(image, 255)
+            if len(image.shape) == 3:
+                image = image.mean(2)
+            image = image[None, ...]
+
+            if self.transform:
+                image = self.transform(image)
+            images.append(image)
+
+        pid = int(patient_dir[3:])
+        return pid, images
