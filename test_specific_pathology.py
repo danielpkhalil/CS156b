92e9d424dfed30234557bdcabdff71c50f764a4a
danielpkhalil <117323103+danielpkhalil@users.noreply.github.com>
2024-05-26 21:26:24 -0400

    Update test_specific_pathology.py

--- test_specific_pathology.py
+++ test_specific_pathology.py
@@ -16,6 +16,7 @@ from torch.utils.data import random_split
 from models.DenseNet import DenseNet121
 from torchvision.transforms import ColorJitter
 from torchvision.transforms import RandomHorizontalFlip, RandomRotation
+import matplotlib.pyplot as plt
 import argparse
 
 def to_rgb(image):
@@ -65,6 +66,15 @@ with torch.no_grad():
         score = output.squeeze().tolist()  # remove batch dimension
         predictions.append(score)
         ids.append(id.item())
+
+        # Apply Grad-CAM and save the image every 100 samples
+        if i % 100 == 0:
+            cam = grad_cam(input_tensor=image, target_category=None)
+            visualization = show_cam_on_image(image.cpu().numpy(), cam.cpu().numpy())
+            # Save the visualization as an image
+            plt.imshow(visualization)
+            plt.axis('off')
+            plt.savefig(f"grad_cam_visualization_{args.pathogen_idx}_{i}.png")
 
 # save sorted predictions in csv
 labels = ['No Finding', 'Enlarged Cardiomediastinum', 'Cardiomegaly', 'Lung Opacity', 'Pneumonia', 'Pleural Effusion',
