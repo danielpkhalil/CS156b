2930e52e3d21eec2aa4774185170899f0629c093
danielpkhalil <117323103+danielpkhalil@users.noreply.github.com>
2024-05-27 20:48:17 -0400

    Update DenseNet.py

--- models/DenseNet.py
+++ models/DenseNet.py
@@ -1,5 +1,6 @@
 import os
 import torch
+import torch.nn as nn
 from torch.utils.data import DataLoader
 from torchvision import transforms
 from torchvision.transforms import ToTensor, Resize, Lambda
