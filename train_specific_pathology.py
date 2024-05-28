6e34b7fa0025b359e841010bbb12d459b64bf490
danielpkhalil <117323103+danielpkhalil@users.noreply.github.com>
2024-05-28 05:55:15 -0400

    Update train_specific_pathology.py

--- train_specific_pathology.py
+++ train_specific_pathology.py
@@ -12,7 +12,7 @@ import pytorch_lightning as pl
 from pytorch_lightning.callbacks import ModelCheckpoint
 from torch.utils.data import random_split
 from models.DenseNet import DenseNet121
-from models.EfficientNetModel import EfficientNet
+from models.EfficientNet import EfficientNetModel
 from torchvision.transforms import ColorJitter
 from torchvision.transforms import RandomHorizontalFlip, RandomRotation, RandomAffine
 import argparse
